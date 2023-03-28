42ad42a35dcbce6d67c4280278f8650b42a5f513
naye-2 <elsagallegos99@gmail.com>
2023-03-28 08:28:43 -0600

    v4

---
+++ openbasicas.py
@@ -0,0 +1,14 @@
+dato1=float(input("Ingrese el dato 1 : "))
+dato2=float(input("Ingresa el dato 2 :"))
+
+sum=dato1+dato2
+resta= dato1-dato2
+multiplicacion=dato1*dato2
+division=dato1/dato2
+
+print("la suma es : ",sum)
+print("La resta es :",resta)
+print("la multiplicacion es:",multiplicacion)
+print("La division es :",division)
+
+
